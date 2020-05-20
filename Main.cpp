b7edaf00cf4e2b000fa22610e3fd8ab8cb09e99b
Adam Varco <adamvarco@live.com>
2020-05-20 14:35:04 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,4 +7,10 @@ int main()
 	
 	//to initialize a Entity pointer and use the parameter contrustor 
 	Entity *en = &Entity(5);
+	
+	
+        //pass the entity pointer to the scopedptr constructor
+	ScopedPtr *st = new ScopedPtr(en);
+	cout << "Entity pointer" << endl;
+	cout << st->GetObj() << endl
 }
